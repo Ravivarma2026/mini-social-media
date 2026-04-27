ec2ce50ac99e25df9d3b42daf52597657b4ac7f3
Ravivarma2026 <rv4544762@gmail.com>
2026-04-27 21:13:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const cors = require("cors");
 const bcrypt = require("bcryptjs");
 
 const app = express();
-
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("Server running"));
 // ================= MIDDLEWARE =================
 app.use(cors());
 app.use(express.json());
